00155ce6340852182d355be7d6bb0b7c636cd238
Nikita Serikov <vorobeu224@mail.ru>
2021-11-07 15:33:42 +0300

    Написал тесты к методам

--- lab2/main_rspec.rb
+++ lab2/main_rspec.rb
@@ -8,4 +8,9 @@ RSpec.describe 'Main' do
   it '#foo test 2' do
     expect(foo(2, 'hhCS')).to eq(16)
   end
+
+  it '#create_pokemons test 1' do
+    allow_any_instance_of(Kernel).to receive(:gets).and_return(2,"Lvysaur","blue","Charmander","orange")
+    expect(create_pokemons).to eq([{"name"=>"Lvysaur", "color"=>"blue"}, {"name"=>"Charmander", "color"=>"orange"}])
+  end
 end
