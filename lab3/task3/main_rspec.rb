925c32a8337f259f0595dcbb084b56e5c7b96a47
Nikita Serikov <vorobeu224@mail.ru>
2021-11-14 17:02:59 +0300

    Исправил тысты в 3 задании

--- lab3/task3/main_rspec.rb
+++ lab3/task3/main_rspec.rb
@@ -3,8 +3,10 @@ require './main'
 RSpec.describe 'Main' do
   it '#third_task' do
     allow_any_instance_of(Kernel).to receive(:gets).and_return("d", "100", "w", "50","b","q")
+    start
     balance = File.read('balance.txt').chomp.to_f
-    expect(balance).to eq(100.0)
+    expect(balance).to eq(150.0)
     allow_any_instance_of(Kernel).to receive(:gets).and_return("w", "50","q")
+    start
   end
 end
